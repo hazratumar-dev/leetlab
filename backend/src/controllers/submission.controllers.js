01caf702841afe3fa1d3b87e41d27874664fd7c6
Hazrat Umar <13234hazrat@gmail.com>
2026-07-27 10:53:45 +0500

    feat: implement submission retrieval endpoints with error handling for not found submissions

--- backend/src/controllers/submission.controllers.js
+++ backend/src/controllers/submission.controllers.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
 import {ApiError} from "../utils/apiError.js"
-import { ApiRsponse } from "../utils/apiResponse.js" 
-import {Submission} from "../models/Submission.models.js"
+import { ApiRsponse } from "../utils/apiResponse.js"
+import { Submission } from "../models/Submission.models.js";
 
 const getAllSubmission = asyncHandler( async(req, res) => {
     const submissions = await Submission.find({
@@ -24,11 +24,53 @@ const getAllSubmission = asyncHandler( async(req, res) => {
 })
 
 // problemId
+
 const getSubmissionsForProblem  = asyncHandler( async(req, res) => {
+    const userId = req.user._id;
+    const { problemId } = req.params;
 
+    const submission = await Submission.find({
+        userId,
+        problemId
+    })
+
+    if(!submission){
+        throw new ApiError(404, "submission not found")
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiRsponse(
+                200,
+                {submission},
+                "fetched submission of the problem"
+            )
+        )
 })
 
-// problemId
 const getAllTheSubmissionsForProblem = asyncHandler( async(req, res) => {
+    const { problemId } = req.params
 
+    const submission = await Submission.countDocuments({ problemId });
+
+    if(!submission){
+        throw new ApiError(404, "submission not found")
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiRsponse(
+                200,
+                {count: submission},
+                "fetched all submission of the problem"
+            )
+        )
 })
+
+export {
+    getAllSubmission,
+    getAllTheSubmissionsForProblem,
+    getSubmissionsForProblem
+}
--- backend/src/routes/submission.routes.js
+++ backend/src/routes/submission.routes.js
@@ -1,6 +1,9 @@
 import {Router} from "express"
-
+import {verifyJWT} from "../middleware/auth.middleware.js"
 const router = Router();
+
+router.use(verifyJWT)
+
 
 
 export default router;
